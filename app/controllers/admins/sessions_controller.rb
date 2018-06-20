9b44538f77569f68f348580782f9efcbe958c2a6
Christian Aracena <CDAracena@gmail.com>
2018-06-20 17:45:36 -0400

    added lessons and languages create pages in admin panels

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -21,7 +21,11 @@ class Admins::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 end
