9e5e5d7ed3a4f52ce3a386317b4ef0c114a57da4
Christian Aracena <CDAracena@gmail.com>
2018-06-25 12:56:46 -0400

    resolved index lessons issue

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,19 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-    let mydiv = document.querySelector('.row .col-4');
+  let mydiv = document.querySelector('.row .col-4');
 
-    mydiv.addEventListener('click', function (e) {
-        id = e.target.id;
-        $('.media1').css('display', 'none');
-        divs = document.querySelector(`.${id}`);
-        if (divs != null) {
+  mydiv.addEventListener('click', function(e) {
+    id = e.target.id;
+    $('.media1').css('display', 'none');
+    divs = document.querySelector(`.${id}`);
+    if (divs != null) {
 
-            $(`.${id}`).css('display', 'block');
-
-        }
-    })
-
+      $(`.${id}`).css('display', 'block');
+    }
+  })
 });
